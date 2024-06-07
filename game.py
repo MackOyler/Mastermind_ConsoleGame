b5612b4ece67cb7f5b4fa10a56f307b7383e10bf
Mack Oyler <120589371+MackOyler@users.noreply.github.com>
2024-06-07 16:15:03 -0500

    game complete

--- game.py
+++ game.py
@@ -55,8 +55,22 @@ def check_code(guess, real_code):
     return correct_pos, incorrect_pos
 
 def game():
+    print(f"Welcome to mastermind, you have {TRIES} to guess the code...")
+    print("The valid colors are", *COLORS)
+    
     code = generate_code()
     for attempts in range(1, TRIES + 1):
         guess = guess_code()
         correct_pos, incorrect_pos = check_code(guess, code)
+        
+        if correct_pos == CODE_LENGTH:
+            print(f"You guessed the code in {attempts} tries!")
+            break 
+        
         print(f"Correct Positions: {correct_pos} | Incorrect Positions: {incorrect_pos}")
+        
+    else:
+        print("You ran out of tries, the code was:", *code)
+        
+if __name__ == "__main__":
+    game()
